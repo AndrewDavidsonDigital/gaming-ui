348095a3ab6c016d124d80267ce452b50231bf73
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-05-17 02:00:22 +1000

    DX: Add css-var hook to dynamically resolve a scale ratio of 16px relative to the current rem

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router';
   import Navigation from '@/components/Navigation.vue';
+  import { computed, onBeforeUnmount, onMounted, ref } from 'vue';
+  import { useDebounce } from './lib/debounce';
+
+  const { debounce } = useDebounce();
+
+  const BASE_FONT_SIZE = 16;
+  const remSize = ref<number>();
+
+
+  onMounted(() => {
+    updateScale();
+    window.addEventListener('resize', () => debounce(() => updateScale()));
+  });
+
+  onBeforeUnmount(() => {
+    window.removeEventListener('resize', () => debounce(() => updateScale()));
+  });
+
+  const scale = computed(() =>  (remSize.value || 1) / BASE_FONT_SIZE);
+
+  function updateScale(){
+    remSize.value = Number(window.getComputedStyle(document.body).fontSize.split('px')[0]);
+  }
+
 </script>
 
 <template>
-  <main class="bg-slate-800 text-green-500 min-h-screen min-w-full">
+  <main
+    class="bg-slate-800 text-green-500 min-h-screen min-w-full"
+    :style="`--dynamic-scale: ${scale};`"
+  >
     <Navigation />
     <RouterView />
   </main>
